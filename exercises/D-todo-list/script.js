db8bce2a2bcfa79b6fe2471981c93704e0458604
Ionut Tripa <ionut.tripa.23@gmail.com>
2021-02-22 23:30:58 +0000

    ToDo List

--- exercises/D-todo-list/script.js
+++ exercises/D-todo-list/script.js
@@ -1,12 +1,45 @@
-function todoList(todos) {
-  // Write your code here...
+let content = document.querySelector('#content');
+let ulEl = document.createElement('ul');
+ulEl.addEventListener('click', function (el) {
+  if (el.target.tagName === 'LI') {
+    el.target.classList.toggle('checked');
+  }
+}, false);
+ulEl.style.height = '100vh';
+ulEl.style.display = 'flex';
+ulEl.style.flexDirection = 'column';
+ulEl.style.justifyContent = 'center';
+ulEl.style.alignItems = 'center';
+content.appendChild(ulEl);
+
+function todoList(array) {
+  for (let elem of array) {
+    let liEl = document.createElement('li');
+    liEl.style.listStyleType = 'none';
+    liEl.innerText = `${elem.todo}`;
+    liEl.style.display = 'block';
+    liEl.style.padding = '5px';
+    liEl.style.margin = '5px';
+    liEl.style.width = '200px';
+    liEl.style.cursor = 'pointer';
+    liEl.style.textAlign = 'center';
+
+    ulEl.appendChild(liEl)
+  }
 }
 
+// document.querySelector('ul li').addEventListener('click', function (elem) {
+//   let elem = document.querySelector('ul li');
+//   elem.style.textDecoration = 'line-through';
+//   elem.style.backgroundColor = 'green';
+// })
+
+
 const todos = [
-  {todo: "wash the dishes"}, 
-  {todo: "walk the dog"}, 
-  {todo: "learn javascript"}, 
-  {todo: "go shopping"}
+  { todo: "wash the dishes" },
+  { todo: "walk the dog" },
+  { todo: "learn javascript" },
+  { todo: "go shopping" }
 ];
 
 todoList(todos);
