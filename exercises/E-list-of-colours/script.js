1a22e0639af75c8cc1a039d85571c14a0b4b1594
Ionut Tripa <ionut.tripa.23@gmail.com>
2021-02-23 09:28:44 +0000

    Finished exercise E-list-of-colors

--- exercises/E-list-of-colours/script.js
+++ exercises/E-list-of-colours/script.js
@@ -1,5 +1,19 @@
-function listOfColours(colours) {
-  // Write your code here...
+let contentEl = document.querySelector('#content');
+let selectEl = document.createElement('select');
+contentEl.appendChild(selectEl);
+let pEl = document.createElement('p');
+contentEl.appendChild(pEl);
+
+function listOfColours(colors) {
+  for (let color of colors) {
+    let optionEl = document.createElement('option');
+    optionEl.innerText = `${color}`;
+    optionEl.addEventListener('click', function () {
+      pEl.innerText = `You have selected: ${color}`;
+      pEl.style.color = `${color}`;
+    })
+    selectEl.appendChild(optionEl);
+  }
 }
 
 const colours = ["red", "blue", "green", "yellow", "pink", "brown"];
