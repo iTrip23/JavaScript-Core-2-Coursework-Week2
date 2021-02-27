d593580870aedb40aec4925ba9179424359eb768
Ionut Tripa <ionut.tripa.23@gmail.com>
2021-02-27 21:41:18 +0000

    finished the function GuessNumber()

--- advanced/1-guess-the-number-project/script.js
+++ advanced/1-guess-the-number-project/script.js
@@ -1,16 +1,41 @@
 let randomNumber = Math.floor(Math.random() * 100 + 1);
+let finalEl = document.querySelector('.final-output');
+let triesEl = document.querySelector('.Tries-output');
+let numOfTries = 7;
+let tries = 1;
+triesEl.innerText = `You have ${numOfTries} tries`;
+console.log(randomNumber);
 
 function guessNumber() {
   //Collect input from the user
   let guess = document.querySelector(".inputs-Values").value;
-
+  if (guess < 1 || guess > 100 || guess.length === 0) {
+    finalEl.innerText = 'Please enter a number between 1 and 100';
+    triesEl.innerText = `You have ${numOfTries}`;
+  }
   //If the user inputs a bad input ie 0, empty string, number greater that 100, number less than zero Print "Please enter a number between 1 and 100"
-
+  if (guess > randomNumber) {
+    numOfTries -= 1;
+    tries++;
+    triesEl.innerText = `You have ${numOfTries}`;
+    finalEl.innerText = 'Number is too high, try again';
+  }
   //If the users guess is higher than the random number print Number is too high, try again (hint use .final-out class to print)
-
+  if (guess < randomNumber) {
+    numOfTries -= 1;
+    tries++;
+    triesEl.innerText = `You have ${numOfTries}`;
+    finalEl.innerText = 'Number is too low, try again';
+  }
   //If the users guess is lower than the random number print Number is too low, try again  (hint use .final-out class to print)
-
+  if (guess == randomNumber) {
+    finalEl.innerText = 'Guess is correct. You win!';
+    triesEl.innerText = `It took you ${tries} tries to get the number`;
+  }
   //If the user has guessed the random number correctly print out the randomNumber with a message "Guess is correct. You win!"
+  if (numOfTries === 0 && guess != randomNumber) {
+    finalEl.innerText = `You lose, the number was ${randomNumber}`;
+  }
 }
 
 // For this task we will be making a "New Game" button function which will reset our game,
